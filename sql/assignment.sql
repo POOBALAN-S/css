870fe977cf820240a9b8ac37ed60331bd18824b0
POOBALAN-S <138599836+POOBALAN-S@users.noreply.github.com>
2023-07-08 19:24:57 +0530

    Update assignment.sql

--- sql/assignment.sql
+++ sql/assignment.sql
@@ -4,3 +4,4 @@ CREATE TABLE todo_list (
   description VARCHAR(255),
   is_completed BOOLEAN
 );
+Create a database called "sales" with a table called "orders" that contains the following columns: order_id (int), customer_id (int), order_date (date), order_total (decimal).
